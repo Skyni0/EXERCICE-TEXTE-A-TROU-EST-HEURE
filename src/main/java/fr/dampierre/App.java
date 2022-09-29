0dcefa1b586ab4457e8cada36e165684db259bdf
Skyni0 <gillot.benju@gmail.com>
2022-09-29 09:00:54 +0200

    heure

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -1,10 +1,33 @@
 package fr.dampierre;
 
+import java.util.Scanner;
+
 public class App {
     
     public static void main(String[] args) {
+        	
+		Scanner clavier = new Scanner(System.in);
 
-        System.out.println();
+	System.out.print("Entrez Heure : ");
+ 	int heure = clavier.nextInt() ;
+
+	System.out.print("Entrez Minute : ");
+	int min = clavier.nextInt();
+
+	System.out.print("Entrez Seconde : ");
+	int sec = clavier.nextInt();
+
+	int heure1= 3600;
+	int min1 = min= 60;
+	int sec1 = sec = 1;
+
+		int total = heure*heure1 + min*min1 + sec*sec1;
+
+	System.out.print("Il s'est écoulé "+ total + " secondes depuis minuit.");
+
+    
     }
+
+
     
 }
